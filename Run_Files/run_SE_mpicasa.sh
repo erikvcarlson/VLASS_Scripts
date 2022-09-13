92a88b35403c854bce84a14e9ce37c5cba55377d
Erik Carlson <78977498+erikvcarlson@users.noreply.github.com>
2022-09-13 13:18:02 -0600

    Update run_SE_mpicasa.sh

--- Run_Files/run_SE_mpicasa.sh
+++ Run_Files/run_SE_mpicasa.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#PBS -V    # Export all environment variables from the qsub commands environment to the batch job.
-#PBS -l pmem=64gb,pvmem=64gb       # Amount of memory needed by each process (ppn) in the job.
-#PBS -d /lustre/aoc/students/ecarlson/data/J0925+1444_Custom_Size/ # Working directory (PBS_O_WORKDIR) set to your Lustre area
-#PBS -l nodes=1:ppn=1 Number of nodes and the number of cores request
-#PBS -m ae                 # Send mail on begin, end and abort
+#SBATCH --exclusive  # Requesting an exlusive node
+#SBATCH -D /lustre/aoc/students/ecarlson/data/J0000+0000/working/ # Working directory (PBS_O_WORKDIR) set to your Lustre area
+#SBATCH --mail-type=END,FAIL                 # Send mail on begin, end and abort
+#SBATCH --mail-user=ecarlson@nrao.edu
+#SBATCH --export=ALL
 
 
 
 
 # casa's python requires a DISPLAY for matplot, so create a virtual X server
 #ensure that the below path is pointed at your command script. 
-xvfb-run -d /home/casa/packages/pipeline/casa-6.4.1-12-pipeline-2022.1.1.5/bin/mpicasa -n 16 --pipeline --nogui --nologger -c  command_script.py
+xvfb-run -d /home/casa/packages/pipeline/casa-6.4.1-12-pipeline-2022.1.1.5/bin/mpicasa -n 17 /home/casa/packages/pipeline/casa-6.4.1-12-pipeline-2022.1.1.5/bin/casa--pipeline --nogui --nologger -c  command_script.py
