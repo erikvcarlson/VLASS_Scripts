c93605fc5850267dd27af89b192128d3940b4ca0
Erik Carlson <78977498+erikvcarlson@users.noreply.github.com>
2022-09-13 13:15:24 -0600

    Update run_SE_slurm.sh

--- Run_Files/run_SE_slurm.sh
+++ Run_Files/run_SE_slurm.sh
@@ -9,4 +9,4 @@
 
 
 # casa's python requires a DISPLAY for matplot, so create a virtual X server
-xvfb-run -d /home/casa/packages/pipeline/casa-6.1.3-3-pipeline-2021.1.1.29/bin/casa --pipeline --nogui --nologger -c  command_script.py
+xvfb-run -d /home/casa/packages/pipeline/casa-6.4.1-12-pipeline-2022.1.1.5/bin/casa --pipeline --nogui --nologger -c  command_script.py
