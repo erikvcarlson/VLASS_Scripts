1d5f7be064e69b719eb24ba5b8d0bc69372fd80d
Erik Carlson <78977498+erikvcarlson@users.noreply.github.com>
2024-04-19 15:05:57 -0400

    Update run_SE_slurm.sh

--- Run_Files/run_SE_slurm.sh
+++ Run_Files/run_SE_slurm.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task=1   # Amount of memory needed by each process (ppn) in the job.
-#SBATCH -D /lustre/aoc/students/ecarlson/data/J0000+0000/working/ # Working directory (PBS_O_WORKDIR) set to your Lustre area
 #SBATCH --mem=64GB
-#SBATCH --mail-type=END,FAIL                 # Send mail on begin, end and abort
-#SBATCH --mail-user=ecarlson@nrao.edu
 #SBATCH --export=ALL
 
 
